6dcfe48e9e95eb37a853cd1d52b11783609a518b
Yicong Li <liyicong.china@gmail.com>
2018-09-24 14:54:10 +1000

    add fragment structure of camera

--- app/src/main/java/com/unimelb/activity/ShareActivity.java
+++ app/src/main/java/com/unimelb/activity/ShareActivity.java
@@ -1,22 +1,31 @@
 package com.unimelb.activity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.unimelb.adapter.ViewPagerAdapter;
+import com.unimelb.base.BaseFragment;
+import com.unimelb.fragment.DiscoverFragment;
 import com.unimelb.instagramlite.R;
+import com.unimelb.utils.BottomNavigationViewHelper;
 import com.unimelb.utils.Permissions;
 
 public class ShareActivity extends AppCompatActivity {
     private static final String TAG = "ShareActivity";
     private Context mContext = ShareActivity.this;
+    private BottomNavigationView navigationView;
+    private ViewPager viewPager;
+    private MenuItem menuItem;
 
     //Constants
     private static final int ACTIVITY_NUM = 2;
@@ -51,6 +60,60 @@ public class ShareActivity extends AppCompatActivity {
         if (!checkPermissionsArray(Permissions.PERMISSIONS)) {
             verifyPermissionsArray(Permissions.PERMISSIONS);
         }
+
+        // setup viewPager
+        viewPager = findViewById(R.id.viewpager_camera);
+        navigationView = findViewById(R.id.navigation_camera);
+        BottomNavigationViewHelper.disableShiftMode(navigationView);
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_library:
+                        viewPager.setCurrentItem(0);
+                        return true;
+                    case R.id.navigation_photo:
+                        viewPager.setCurrentItem(1);
+                        return true;
+                }
+
+                return false;
+            }
+        });
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (menuItem != null) {
+                    menuItem.setChecked(false);
+                } else {
+                    navigationView.getMenu().getItem(0).setChecked(false);
+                }
+                menuItem = navigationView.getMenu().getItem(position);
+                menuItem.setChecked(true);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+
+        setupViewPager(viewPager);
+    }
+
+    // setup the content of viewPager
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        adapter.addFragment(BaseFragment.newInstance("library"));
+        adapter.addFragment(BaseFragment.newInstance("photo"));
+        viewPager.setAdapter(adapter);
     }
 
     /**
