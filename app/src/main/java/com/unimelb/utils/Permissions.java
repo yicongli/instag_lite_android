1ba00639d6d2230d0a09e9b7ee04f856f558c26b
Yicong Li <liyicong.china@gmail.com>
2018-09-24 14:24:09 +1000

    Add permission relevant files

---
+++ app/src/main/java/com/unimelb/utils/Permissions.java
@@ -0,0 +1,13 @@
+package com.unimelb.utils;
+
+import android.Manifest;
+
+public class Permissions {
+
+    public static final String[] PERMISSIONS = {
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+
+    };
+}
