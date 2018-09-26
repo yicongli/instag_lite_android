f94110f0f786d339f84131638c98c41f7326593b
Di Wang <wangdicoder@gmail.com>
2018-09-26 22:36:04 +1000

    dev: build search fragment

--- app/src/main/java/com/unimelb/fragment/DiscoverFragment.java
+++ app/src/main/java/com/unimelb/fragment/DiscoverFragment.java
@@ -1,5 +1,6 @@
 package com.unimelb.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,7 +8,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
+import com.unimelb.activity.SearchActivity;
 import com.unimelb.instagramlite.R;
 import com.unimelb.net.HttpRequest;
 import com.unimelb.net.IResponseHandler;
@@ -19,13 +22,16 @@ public class DiscoverFragment extends Fragment {
 
 
     public DiscoverFragment() {
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_discover, container, false);
+        LinearLayout linearLayout = view.findViewById(R.id.search_bar);
+        linearLayout.setOnClickListener((view1) -> {
+            startActivity(new Intent(this.getContext(), SearchActivity.class));
+        });
 
         return view;
     }
