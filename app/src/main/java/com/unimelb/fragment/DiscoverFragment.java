feab924a13c3d2d361e2da6a45bf1c564364f62e
Di Wang <wangdicoder@gmail.com>
2018-10-18 12:11:52 +1100

    dev: refactor discover fragment

--- app/src/main/java/com/unimelb/fragment/DiscoverFragment.java
+++ app/src/main/java/com/unimelb/fragment/DiscoverFragment.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,16 +16,21 @@ import android.widget.LinearLayout;
 import com.scwang.smartrefresh.header.WaterDropHeader;
 import com.scwang.smartrefresh.layout.api.RefreshLayout;
 import com.unimelb.activity.SearchActivity;
+import com.unimelb.adapter.SearchListAdapter;
 import com.unimelb.adapter.SquareImageAdapter;
+import com.unimelb.entity.BasicUserProfile;
 import com.unimelb.instagramlite.R;
 import com.unimelb.net.HttpRequest;
 import com.unimelb.net.IResponseHandler;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Discover fragment.
  */
 public class DiscoverFragment extends Fragment {
-
+    private List<BasicUserProfile> suggestionList;
 
     public DiscoverFragment() {
     }
@@ -32,9 +39,35 @@ public class DiscoverFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_discover, container, false);
+        initData();
         initView(view);
 
         return view;
+    }
+
+    private void initData() {
+        suggestionList = new ArrayList<>();
+        String[] imageUrls = new String[]{
+                "http://pf3on5bei.sabkt.gdipper.com/profile18.jpg",
+                "http://pf3on5bei.sabkt.gdipper.com/profile20.jpg",
+                "http://pf3on5bei.sabkt.gdipper.com/profile25.jpg",
+                "http://pf3on5bei.sabkt.gdipper.com/profile17.jpg",
+                "http://pf3on5bei.sabkt.gdipper.com/profile30.jpg",
+                "http://pf3on5bei.sabkt.gdipper.com/profile31.jpg",
+                "http://pf3on5bei.sabkt.gdipper.com/profile28.jpg",
+                "http://pf3on5bei.sabkt.gdipper.com/profile27.jpg",
+                "http://pf3on5bei.sabkt.gdipper.com/profile26.jpg",
+                "http://pf3on5bei.sabkt.gdipper.com/profile14.jpg",
+                "http://pf3on5bei.sabkt.gdipper.com/profile15.jpg",
+                "http://pf3on5bei.sabkt.gdipper.com/profile13.jpg",
+                "http://pf3on5bei.sabkt.gdipper.com/profile9.jpg",
+                "http://pf3on5bei.sabkt.gdipper.com/profile2.jpg",
+                "http://pf3on5bei.sabkt.gdipper.com/profile10.jpg",
+        };
+        for (String url : imageUrls) {
+            BasicUserProfile profile = new BasicUserProfile(url, "Test", "test username");
+            suggestionList.add(profile);
+        }
     }
 
     public void initView(View view) {
@@ -50,22 +83,9 @@ public class DiscoverFragment extends Fragment {
             layout.finishLoadMore(2000/*,false*/);// false means false
         });
 
-        GridView gridView = view.findViewById(R.id.search_grid_view);
-
-        final String[] imageUrls = new String[]{
-               "http://pf3on5bei.sabkt.gdipper.com/1dbbf32d-8a1f-4194-a797-075dfcdbba38.jpeg",
-               "http://pf3on5bei.sabkt.gdipper.com/1dbbf32d-8a1f-4194-a797-075dfcdbba38.jpeg",
-               "http://pf3on5bei.sabkt.gdipper.com/24e3c68b-4042-4988-bfd9-694328a8b52c.jpeg",
-               "http://pf3on5bei.sabkt.gdipper.com/1dbbf32d-8a1f-4194-a797-075dfcdbba38.jpeg",
-               "http://pf3on5bei.sabkt.gdipper.com/1dbbf32d-8a1f-4194-a797-075dfcdbba38.jpeg",
-               "http://pf3on5bei.sabkt.gdipper.com/1dbbf32d-8a1f-4194-a797-075dfcdbba38.jpeg",
-               "http://pf3on5bei.sabkt.gdipper.com/1dbbf32d-8a1f-4194-a797-075dfcdbba38.jpeg",
-               "http://pf3on5bei.sabkt.gdipper.com/24e3c68b-4042-4988-bfd9-694328a8b52c.jpeg",
-               "http://pf3on5bei.sabkt.gdipper.com/1dbbf32d-8a1f-4194-a797-075dfcdbba38.jpeg",
-        };
-
-        SquareImageAdapter adapter = new SquareImageAdapter(this.getContext(), imageUrls);
-        gridView.setAdapter(adapter);
+        RecyclerView listView = view.findViewById(R.id.search_suggestion_list);
+        listView.setLayoutManager(new LinearLayoutManager(this.getContext()));
+        listView.setAdapter(new SearchListAdapter(this.getContext(), suggestionList));
     }
 
 
